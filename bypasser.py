714803b83cf7fdae94a503d247e2aabab6860d72
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-02-09 10:23:26 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1710,7 +1710,7 @@ def shortners(url):
         return try2link_bypass(url)
 
     # urlsopen
-    elif "https://urlsopen.com/" in url:
+    elif "https://urlsopen.net/" in url:
         print("entered urlsopen:",url)
         return urlsopen(url)
 
