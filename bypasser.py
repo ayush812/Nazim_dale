475343bc826c920e391b3f2a45a98b250bc3a779
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-02-12 12:04:19 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1664,9 +1664,13 @@ def gtlinks(url: str) -> str:
  
 
 def lksfy(url):
-    client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://lksfy.site/"
-    ref = "https://kalvidudes.in/" 
+    client = cloudscraper.create_scraper(allow_brotli=False) 
+    if 'https://lksfy.com/' in url:
+        DOMAIN = "https://kalvidudes.in/"
+    elif 'https://kalvidudes.in/' in url:
+    	  DOMAIN = "https://frequet.com/"
+    else:
+    	 ref = "https://lksfy.site/" 
     h = {"referer": ref}
     resp = client.get(url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
