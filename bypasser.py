4e2f6718ceb3961b3fa1add347f2e3b7abb6c668
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-03-10 20:06:59 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1168,6 +1168,27 @@ def gplinks(url: str) -> str:
 	bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
 	return bypassed_url
 
+##############################################################################################        
+# shrink
+
+def shrink(url):
+    client = requests.session()
+    DOMAIN = "https://shrinkforearn.in"
+    url = url[:-1] if url[-1] == '/' else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://finance.uploadsoon.com"
+    h = {"referer": ref}
+    resp = client.get(final_url,headers=h)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input")
+    data = { input.get('name'): input.get('value') for input in inputs }
+    h = { "x-requested-with": "XMLHttpRequest" }
+    time.sleep(7)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
 
 ######################################################################################################
 # droplink
@@ -1959,6 +1980,10 @@ def shortners(url):
         print("entered mdiskshortner:",url)
         return mdiskshortner(url)
         
+# oggylink
+    elif "https://shrinkforearn.in" in url:
+        print("entered shrinkforearn:",url)
+        return shrink(url)
 # oggylink
     elif "https://oggylink.com/" in url:
         print("entered oggylink:",url)
