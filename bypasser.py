a040ec5385a2678bf529024cbdd9e885a7a7e29c
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-03-10 20:12:15 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1752,11 +1752,11 @@ def lksfy(url):
 	
 def indi(url):
     client = requests.session()
-    DOMAIN = "https://file.earnash.com/"
+    DOMAIN = "https://file.earnash.com//"
     url = url[:-1] if url[-1] == '/' else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    ref = "https://x.earnash.com/"
+    ref = "https://earn.cordtpoint.co.in"
     h = {"referer": ref}
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
