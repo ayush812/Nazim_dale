8c4a72ec006207d3209b88d75756e3c59f7cf047
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-03-24 07:42:33 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1542,7 +1542,7 @@ def urlsopen(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
     
     
-    DOMAIN = "https://blogpost.viewboonposts.com/ssssss//"
+    DOMAIN = "https://blogpost.viewboonposts.com/ssssssagasdgeardgaegaqe//"
 
     url = url[:-1] if url[-1] == '/' else url
 
