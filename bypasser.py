91e0ea3bc65a47ae0b75aae61eaaa77d17c8e6ee
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-04-12 21:24:28 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1180,19 +1180,12 @@ def adfly(url):
 def tinyfy(url):
    
     client = requests.session()
-    
     DOMAIN = "https://tinyfy.in"
-
     url = url[:-1] if url[-1] == '/' else url
-
     code = url.split("/")[-1]
-    
     final_url = f"{DOMAIN}/{code}"
-    
-    ref = "https://www.yotrickslog.tech/"
-    
+    ref = "https://www.yotrickslog.tech"
     h = {"referer": ref}
-  
     resp = client.get(final_url,headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
     inputs = soup.find_all("input")
