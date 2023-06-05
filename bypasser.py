8f9e81d583c657bd513fbf149877eda24fea3a84
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-06-05 09:10:36 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -955,21 +955,27 @@ def shortingly(url):
 # Gyanilinks - gtlinks.me
 
 def gyanilinks(url):
-    DOMAIN = "https://tech.hipsonyc.com/"
     client = cloudscraper.create_scraper(allow_brotli=False)
-    url = url[:-1] if url[-1] == '/' else url
+    DOMAIN = "https://go.gyanitheme.com"
+    ref = "https://www.hipsonyc.com/"
+    h = {"referer": ref}
+    url = url[:-1] if url[-1] == "/" else url
     code = url.split("/")[-1]
     final_url = f"{DOMAIN}/{code}"
-    resp = client.get(final_url)
+    resp = client.get(final_url, headers=h)
     soup = BeautifulSoup(resp.content, "html.parser")
-    try: inputs = soup.find(id="go-link").find_all(name="input")
-    except: return "Incorrect Link"
-    data = { input.get('name'): input.get('value') for input in inputs }
-    h = { "x-requested-with": "XMLHttpRequest" }
+    try:
+        inputs = soup.find(id="go-link").find_all(name="input")
+    except BaseException:
+        return "Incorrect Link"
+    data = {input.get("name"): input.get("value") for input in inputs}
+    h = {"x-requested-with": "XMLHttpRequest"}
     time.sleep(5)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
-    try: return r.json()['url']
-    except: return "Something went wrong :("
+    try:
+        return r.json()["url"]
+    except BaseException:
+        return "Something went wrong :(
 
 
 #######################################################
