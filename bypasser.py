10c35fe8555c20750ba3d86efd327d53a523b9df
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-06-28 13:15:03 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -191,6 +191,27 @@ def onepage(url):
     try:
         return r.json()['url']
     except: return "Something went wrong :("
+###############################################################
+# vipurl
+def vipurl(url):
+    
+    client = requests.session()
+    DOMAIN = "https://count.vipurl.in"
+    url = url[:-1] if url[-1] == '/' else url
+    code = url.split("/")[-1]
+    final_url = f"{DOMAIN}/{code}"
+    ref = "https://loanhelpful.net/"
+    h = {"referer": ref}
+    resp = client.get(final_url,headers=h) 
+    soup = BeautifulSoup(resp.content, "html.parser")
+    inputs = soup.find_all("input") 
+    data = { input.get('name'): input.get('value') for input in inputs }
+    h = { "x-requested-with": "XMLHttpRequest" }
+    time.sleep(5)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+    try:
+        return r.json()['url']
+    except: return "Something went wrong :("
     
 ###############################################################
 # psa 
@@ -2026,7 +2047,12 @@ def shortners(url):
     elif "https://psa.pm/" in url:
         print("entered psa:",url)
         return psa_bypasser(url)
-        
+
+ # vipurl
+    elif "https://link.vipurl.in/" in url:
+        print("entered vipurl:",url)
+        return vipurl(url)
+	    
     # sharer pw
     elif "https://sharer.pw/" in url:
         if XSRF_TOKEN == "" or Laravel_Session == "":
