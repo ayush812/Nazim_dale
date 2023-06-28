82c494c3d0fd84d380ef446480875e8acc05b928
ayush812 <120151530+ayush812@users.noreply.github.com>
2023-06-28 12:28:55 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -1743,18 +1743,32 @@ def earnl(url):
 # moneykamalo
 
 def moneykamalo(url):
+    
     client = requests.session()
+    
+    
     DOMAIN = "https://go.moneykamalo.com"
+
     url = url[:-1] if url[-1] == '/' else url
+
     code = url.split("/")[-1]
+    
     final_url = f"{DOMAIN}/{code}"
+    
     ref = "https://techkeshri.com/"
+    
     h = {"referer": ref}
+  
     resp = client.get(final_url,headers=h)
+    
     soup = BeautifulSoup(resp.content, "html.parser")
+    
     inputs = soup.find_all("input")
+   
     data = { input.get('name'): input.get('value') for input in inputs }
+
     h = { "x-requested-with": "XMLHttpRequest" }
+    
     time.sleep(5)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
